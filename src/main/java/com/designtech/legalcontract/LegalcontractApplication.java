6e51bb61748264353f1561e72f6a4d84dc1bf655
Heber Cordova Jimenez <u201924464@upc.edu.pe>
2022-09-28 11:43:05 -0500

    RestTemplate for Lawyer and Client

--- src/main/java/com/designtech/legalcontract/LegalcontractApplication.java
+++ src/main/java/com/designtech/legalcontract/LegalcontractApplication.java
@@ -2,6 +2,8 @@ package com.designtech.legalcontract;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LegalcontractApplication {
@@ -10,4 +12,8 @@ public class LegalcontractApplication {
 		SpringApplication.run(LegalcontractApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
